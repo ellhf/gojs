ee9dcbe47f655ca1d3d19d8bc3c7c331dcf2f756
Haifeng Liu <velkinta@gmail.com>
2021-03-17 14:42:24 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import * as db from './db.js';
 import * as updater from './updater.js';
 
 (async () => {
-  await db.sequelize.sync({ force: true });
+  // await db.sequelize.sync({ force: true });
   // await GameMeta.create({ id: 'HP9000-CUSA03023_00-BLOODBORNE0000AS' })
   updater.run();
   app.listen(8001);
